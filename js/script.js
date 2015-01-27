1ed3ea0882e2f9fe3a2610c1e41f116f32ffbce0
Alfredo Miranda <alfredo@miranda.io>
2015-01-27 15:00:04 -0600

    Add script.js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(function () {
+  var NUM_BANDS = 5;
+
+  function buildBandItems(num) {
+    var i;
+    var ul = $('ul');
+    var li = '<li><span class="band-name"></span>' +
+                    '<span class="mentions-container">' +
+                      '<span class="num-mentions"></span>' +
+                      '<span class="mention">Mentions</span></span></li>';
+
+    for(i = 0; i < num; i++) {
+      ul.append(liStr);
+    }
+  }
+
+  function updateBands(bands) {
+    var i, len;
+    bandNames = $('.band-name');
+    numMentions = $('.num-mentions');
+
+    for(i = 0, len = bands.length; i < len; i++) {
+      bandNames[i].textContent = bands[0].name;
+      numMentions[i].textContent = bands[0].count;
+    }
+  }
+
+  var poller = new massrel.Poller({frequency: NUM_BANDS, limit: 5},
+    updateBands);
+  poller.start();
+});
