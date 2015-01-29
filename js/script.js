63dca15180ce35cccc2c27663c584bd0b521a5ca
Alfredo Miranda <alfredo@miranda.io>
2015-01-28 19:30:28 -0600

    Minor improvements

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $(function () {
 
   function buildBandItems(num) {
     var i;
-    var ul = $('ul');
     var li = '<li>' +
                '<span class="band-name"></span>' +
                '<span class="mentions-container">' +
@@ -19,12 +18,8 @@ $(function () {
 
   function updateBands(bands) {
     var i, len;
-    var ul = $('ul');
 
     ul.fadeOut('slow', function () {
-      var bandNames = $('.band-name');
-      var numMentions = $('.num-mentions');
-      
       for(i = 0, len = bands.length; i < len; i++) {
         bandNames[i].textContent = bands[i].name;
         numMentions[i].textContent = bands[i].count;
@@ -34,7 +29,10 @@ $(function () {
     });
   }
 
+  var ul = $('ul');
   buildBandItems(NUM_BANDS);
+  var bandNames = $('.band-name');
+  var numMentions = $('.num-mentions');
   var poller = new massrel.Poller({frequency: 15, limit: NUM_BANDS},
     updateBands);
   poller.start();
